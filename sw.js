c4ca8ce98fae47a3f9e091878dfc851cb3b38104
CaminoPete <prvcmo@gmail.com>
2026-08-10 07:17:54 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "trip-budget-tracker-v60";
+const CACHE_NAME = "trip-budget-tracker-v55";
 
 const APP_FILES = [
   "./",
   "./index.html",
-  "./css/styles.css?v=60",
-  "./js/app.js?v=60",
+  "./css/styles.css?v=55",
+  "./js/app.js?v=55",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
